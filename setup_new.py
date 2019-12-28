daf9a7f8644f62aeaa3813c3379e369f1223e594
Florian FELICE <florian.felice@outlook.com>
2019-12-28 15:41:09 +0100

    Upload version 1.1.17 to pypi

--- setup_new.py
+++ setup_new.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pycof",
-    version="1.0.9",
+    version="1.1.17",
     author="Florian Felice",
     author_email="florian.felice@outlook.com",
     description="A package for commonly used functions",
