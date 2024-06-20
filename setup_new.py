b47f3166d4d6aab1c2b595bb0b7af6ebbd468625
Florian Felice <admin@florianfelice.com>
2024-06-20 16:33:05 +0200

    Upload version 1.5.8 to pypi. Minor changes

--- setup_new.py
+++ setup_new.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pycof",
-    version="1.5.7",
+    version="1.5.8",
     author="Florian Felice",
     author_email="admin@florianfelice.com",
     description="A package for commonly used functions",
