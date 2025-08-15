8f263a5e8a32cd6206e88abf88472d9fb654cf2d
Florian Felice <admin@florianfelice.com>
2025-08-15 14:35:18 +0200

    Upload version 1.5.14 to pypi. feat: setup_logging from base pc

--- pycof/init_template.py
+++ pycof/init_template.py
@@ -3,5 +3,6 @@ from .data import *
 from .format import *
 from .misc import _get_config as get_config
 from .misc import _pycof_folders as pycof_folders
+from .misc import setup_logging
 
 __version__ = '{version}'
